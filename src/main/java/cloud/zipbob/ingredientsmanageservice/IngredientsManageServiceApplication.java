87509b25b20a7b823a9617cb9e55333576339fc3
Kyumin-Chang <kyumin0529@naver.com>
2024-11-05 20:10:55 +0900

    feat: Ingredients-manage-service Init

---
+++ src/main/java/cloud/zipbob/ingredientsmanageservice/IngredientsManageServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.zipbob.ingredientsmanageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientsManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientsManageServiceApplication.class, args);
+    }
+
+}
