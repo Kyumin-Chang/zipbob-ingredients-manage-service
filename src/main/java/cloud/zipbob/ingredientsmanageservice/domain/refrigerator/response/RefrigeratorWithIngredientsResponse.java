48a6f9eda9f77a54b117d6b69088e7b116ce70ce
Kyumin-Chang <kyumin0529@naver.com>
2024-12-26 02:18:14 +0900

    chore: 직렬화 관련 수정

--- src/main/java/cloud/zipbob/ingredientsmanageservice/domain/refrigerator/response/RefrigeratorWithIngredientsResponse.java
+++ src/main/java/cloud/zipbob/ingredientsmanageservice/domain/refrigerator/response/RefrigeratorWithIngredientsResponse.java
@@ -4,15 +4,12 @@ import cloud.zipbob.ingredientsmanageservice.domain.ingredient.Ingredient;
 import cloud.zipbob.ingredientsmanageservice.domain.refrigerator.Refrigerator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
-public class RefrigeratorWithIngredientsResponse implements Serializable {
+public class RefrigeratorWithIngredientsResponse {
     private Long refrigeratorId;
     private Long memberId;
     private List<Ingredient> ingredients;
